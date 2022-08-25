ee3cf2ba354a0dfd8a67a096bb07cc31c832ce77
Warsame <warsameahmed95@gmail.com>
2022-08-26 00:12:51 +0100

    Completed removeFromArray task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
+    let lst = arguments[0];
+    for(let i = 1; i <= arguments.length - 1; i++){
+        let index = lst.indexOf((arguments[i]));
+        if(index >= 0){
+            console.log(index);
+            lst.splice(index,1);
+        }
+    }
 
+    return lst;
 };
 
 // Do not edit below this line
