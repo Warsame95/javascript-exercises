a8e738f8bbfb2298227a00ae9215b3121ea73e46
Warsame <warsameahmed95@gmail.com>
2022-08-26 01:23:26 +0100

    Completed sumAll task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let total = 0;
+    let max;
+    let min;
+    if (typeof start != 'number' || typeof end != 'number'){
+        return "ERROR";
+     }
 
+     if (start < 0 || end < 0){
+        return "ERROR";
+     }
+
+     if (start > end){
+        min = end;
+        max = start;
+     }
+     else{
+        min = start;
+        max = end;
+     }
+
+    for (let i = min; i <= max; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
