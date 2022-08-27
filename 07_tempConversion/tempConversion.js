d40b0c6b8d608dcf9293155ac4cbb33e1486658f
Warsame <warsameahmed95@gmail.com>
2022-08-27 10:17:37 +0100

    Completed tempConversion task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahr) {
+  let celcius = (fahr - 32) * (5/9);
+  return Math.round(celcius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let fahr = (celcius * (9/5)) + 32;
+  return Math.round(fahr * 10) / 10;
 };
 
 // Do not edit below this line
