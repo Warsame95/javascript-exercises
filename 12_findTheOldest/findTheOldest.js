353c6e33c9609c1e690a2e888264f6d8615f2a45
Warsame <warsameahmed95@gmail.com>
2022-10-24 01:02:40 +0100

    implemented function using map sort and at functions

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const date = new Date();
 
+    
+
+
+    const oldest = people.map(function(person){
+        if (person.yearOfDeath == undefined){
+            person.yearOfDeath = date.getFullYear();
+        }
+        return person;
+    }).sort(function(a,b){
+        if (a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth){
+            return 1
+        }
+        else{
+            return -1;
+        }
+    }).at(0);
+    console.log(oldest);
+    return oldest;
 };
 
 // Do not edit below this line
