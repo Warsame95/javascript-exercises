983133665dc18eb01a72fcab141b122eaebdd07b
Warsame <warsameahmed95@gmail.com>
2022-10-25 22:04:04 +0100

    Implemented palindrome function and passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let reverse = str.replaceAll(",","").replaceAll("!","").replaceAll(".","").replaceAll(" ","").toLowerCase().split("").reverse().join("");
+    console.log(reverse);
+    return reverse == str.replaceAll(",","").replaceAll("!","").replaceAll(".","").replaceAll(" ","").toLowerCase();
 };
 
 // Do not edit below this line
